8e06b0a4827e44849d8883a342512f342d55ac38
ilushareg <ilushareg@gmail.com>
2017-06-08 23:35:07 +0300

    initial commit

---
+++ Assets/mainscript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("a_UI", UnityEngine.SceneManagement.LoadSceneMode.Additive);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/testtint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testtint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    }
+    private void Awake()
+    {
+        UnityEngine.Color c = new UnityEngine.Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.SetColor("_Tint", c);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
